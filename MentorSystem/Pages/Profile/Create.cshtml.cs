b0c76217008f826a129a8d379bd471943869a7b7
LeNamPhu <105411107+LeNamPhu@users.noreply.github.com>
2023-03-13 17:51:33 +0700

    change ui login + view class

---
+++ MentorSystem/Pages/Profile/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repository.Models;
+
+namespace MentorSystem.Pages.Profile
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Repository.Models.MentorSystemContext _context;
+
+        public CreateModel(Repository.Models.MentorSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Account Account { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Accounts.Add(Account);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
